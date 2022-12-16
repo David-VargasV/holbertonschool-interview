769c44e2625d3a02ebcf8544d85aa3405f2bf8bf
David-vargasV <4523@holbertonschool.com>
2022-12-16 13:39:57 -0500

    Minioperations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,4 +9,11 @@ def minOperations(n):
     number of operations needed to resultin
     exactly n H characters in the file.
     """
-    
+    opertation = 0
+    div = 2
+    while n > 1:
+        while n % div == 0:
+            opertation += div
+            n = int(n / div)
+        div += 1
+    return (opertation)
