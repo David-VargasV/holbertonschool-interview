35d9c00a35c3c21073f7ee30d41dd9906420dbc8
David-vargasV <4523@holbertonschool.com>
2022-12-16 13:22:56 -0500

    Minimum_Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Minimum Operations """
+
+
+def minOperations(n):
+    """
+    Method that calculates the fewest
+    number of operations needed to resultin
+    exactly n H characters in the file.
+    """
+    
